99847151d0dd019018f40eca8a62784512ec5ae6
AngieV <angiev@me.com>
2021-09-19 17:17:54 -0500

    Complete models with inheritance

---
+++ src/main/java/org/launchcode/Songs4Soldiers/models/Purchaser.java
@@ -0,0 +1,10 @@
+package org.launchcode.Songs4Soldiers.models;
+
+public class Purchaser extends User{
+
+    public Purchaser(int userID, String name, String email, String phone, String userType) {
+        super(userID, name, email, phone, userType);
+
+    }
+
+}
--- src/main/java/org/launchcode/Songs4Soldiers/models/Volunteer.java
+++ src/main/java/org/launchcode/Songs4Soldiers/models/Volunteer.java
@@ -1,4 +1,28 @@
 package org.launchcode.Songs4Soldiers.models;
 
-public class Volunteer {
+import java.util.Date;
+
+public class Volunteer extends User{
+
+    private String vol_help;
+
+    public Volunteer(int userID, String name, String email, String phone, String userType, String vol_help) {
+        super(userID, name, email, phone, userType);
+        this.vol_help = vol_help;
+    }
+
+    public String getVol_help() {
+        return vol_help;
+    }
+
+    public void setVol_help(String vol_help) {
+        this.vol_help = vol_help;
+    }
+
+    @Override
+    public String toString() {
+        return "Volunteer{" +
+                "vol_help='" + vol_help + '\'' +
+                '}';
+    }
 }
