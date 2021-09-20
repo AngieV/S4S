9e32acb80055f4331d3941f0665fb7f78fccec9c
AngieV <angiev@me.com>
2021-09-19 21:01:16 -0500

    update Veteran Model

--- src/main/java/org/launchcode/Songs4Soldiers/models/Veteran.java
+++ src/main/java/org/launchcode/Songs4Soldiers/models/Veteran.java
@@ -1,4 +1,68 @@
 package org.launchcode.Songs4Soldiers.models;
 
-public class Veteran {
+import java.util.Date;
+
+public class Veteran extends User{
+
+
+    private String branch;
+
+    private Date vet_serviceStart;
+
+    private Date vet_serviceEnd;
+
+    //@Size(max=500, message = "Description too long!")
+    private String vet_help;
+
+    public Veteran(int userID, String name, String email, String phone, String userType, String branch, Date vet_serviceStart, Date vet_serviceEnd, String vet_help) {
+        super(userID, name, email, phone, userType);
+        this.branch = branch;
+        this.vet_serviceStart = vet_serviceStart;
+        this.vet_serviceEnd = vet_serviceEnd;
+        this.vet_help = vet_help;
+    }
+
+    public Veteran(){}
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public Date getVet_serviceStart() {
+        return vet_serviceStart;
+    }
+
+    public void setVet_serviceStart(Date vet_serviceStart) {
+        this.vet_serviceStart = vet_serviceStart;
+    }
+
+    public Date getVet_serviceEnd() {
+        return vet_serviceEnd;
+    }
+
+    public void setVet_serviceEnd(Date vet_serviceEnd) {
+        this.vet_serviceEnd = vet_serviceEnd;
+    }
+
+    public String getVet_help() {
+        return vet_help;
+    }
+
+    public void setVet_help(String vet_help) {
+        this.vet_help = vet_help;
+    }
+
+    @Override
+    public String toString() {
+        return "Veteran{" +
+                "branch='" + branch + '\'' +
+                ", vet_serviceStart=" + vet_serviceStart +
+                ", vet_serviceEnd=" + vet_serviceEnd +
+                ", vet_help='" + vet_help + '\'' +
+                '}';
+    }
 }
