f839f65912fb7cdcd1e72ce3297c6ea461bc02a3
AngieV <angiev@me.com>
2021-09-21 19:50:47 -0500

    add Repository

--- src/main/java/org/launchcode/Songs4Soldiers/data/UserRepository.java
+++ src/main/java/org/launchcode/Songs4Soldiers/data/UserRepository.java
@@ -1,8 +1,9 @@
 package org.launchcode.Songs4Soldiers.data;
 
 import org.launchcode.Songs4Soldiers.models.User;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
