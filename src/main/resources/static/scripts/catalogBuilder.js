f60c8c47f2789fbacda8eeebacfe08a774bf7b01
AngieV <angiev@me.com>
2021-09-25 22:29:29 -0500

    edit catalogBuilder to pass JSON lint, hardcode merch until issues are resolved

--- src/main/resources/static/scripts/catalogBuilder.js
+++ src/main/resources/static/scripts/catalogBuilder.js
@@ -1,133 +1,85 @@
 var products = {
-    "shirts": [ // array
-        {
-            "name": "Crewneck Sweatshirt",
-            "size": ["S", "M", "L", "XL", "2X"],
-            "color": "Heather grey",
-            "price": "30.00",
-            "description": ["Celebrate, Honor and Remember with this Songs4Soldiers Crewneck Sweatshirt",
-                            "Soft, Lightweight French Terry Material",
-                            "66/34 polyester/ringspun cotton",
-                            "Black Heather Fabric Color",
-                            "Preshrunk Adult Sizes"],
-            "image": "@{/images/catalog/CrewSweatshirt_740x.jpeg}",
-            "images_expanded": "sweatshirtModels_740x.jpeg"
-        },
-        {
-            "name": "Tailgate Fest Baseball Tee",
-            "size": ["S", "M", "L", "XL", "2X"],
-            "color": "White/Maroon",
-            "price": "25.00",
-            "description": ["The baseball-style T-shirt is from the 2020 Drive-in Concert, benefitting Songs4Soldiers",
-                            "52% cotton / 48% polyester",
-                            "Unisex, regular fit t-shirt"],
-            "image": ["TailgateFestTee-front_740x.jpeg", "TailgateFestTee-back_740x"]
-        },
-        {
-            "name": "2020 Event (Cancelled) Tee",
-            "size": ["S", "M", "L", "XL", "2X"],
-            "color": "Dark Heather grey",
-            "price": "15.00",
-            "description": "",
-            "images": ""
-        },
-        {
-            "name": "2019 Event Tee",
-            "size": ["S", "M", "L", "XL", "2X"],
-            "color": "Ash grey",
-            "price": "10.00",
-            "description": "Light, soft-touch, premium fabric racerback tank top - part of the S4S standard issue.",
-                            "9% cotton / 91% polyester", "Women's flared cut", "Racerback tank top"
-            "images": ""
-        },
-        {
-            "name": "2019 Youth Event Tee",
-            "size": ["S", "M", "L", "XL", "2X"],
-            "color": "Ash grey",
-            "price": "10.00",
-            "description": "Light, soft-touch, premium fabric racerback tank top - part of the S4S standard issue.",
-                            "9% cotton / 91% polyester", "Women's flared cut", "Racerback tank top"
-            "images": ""
-        },
-        {
-            "name": "S4S Womens Tank Top",
-            "size": ["S", "M", "L", "XL", "2X"],
-            "color": "Black heather",
-            "price": "20.00",
-            "description": "Light, soft-touch, premium fabric racerback tank top - part of the S4S standard issue.",
-                            "9% cotton / 91% polyester", "Women's flared cut", "Racerback tank top"
-            "images": ""
-        }
-    ],
-    "hats": [{
-            "name": "B&W Baseball Cap",
-            "color": "Black/White",
-            "price": "20.00",
-            "images": ""
-        },
-        {
-            "name": "Black Flatbill Cap",
-            "color": "Black",
-            "price": "",
-            "description": "",
-            "images": ""
-        },
-        {
-            "name": "Trucker Visor",
-            "color": "Denim/Putty",
-            "price": "15.00",
-            "description": "",
-            "images": ""
-        }
-    ],
-    "misc": [{
-                "name": "S4S Facemask",
-                "color": "Mobile Design and Usability for iOS",
-                "price": "10.00",
-                "description": "",
-                "images": ""
-            },
-            {
-                "name": "S4S Sunglasses",
-                "color": "Blue",
-                "price": "5.00",
-                "description": "",
-                "images": ""
-            },
-            {
-                "name": "S4S Fanny Pack",
-                "color": "Blue",
-                "price": "10.00",
-                "description": "",
-                "images": ""
-            }
-            {
-                "name": "S4S Insulated Tumbler",
-                "color": "white",
-                "price": "20.00",
-                "description": "",
-                "images": ""
-            },
-            {
-                "name": "S4S Insulated Convertible Cup",
-                "color": "white",
-                "price": "20.00",
-                "description": "",
-                "images": ""
-            },
-            {
-                "name": "S4S Decals (3)",
-                "color": "Black",
-                "price": "5.00",
-                "description": "",
-                "images": ""
-            },
-            {
-                "name": "S4S Decals (1)",
-                "color": "Black",
-                "price": "2.00",
-                "description": "",
-                "images": ""
-            }
-        ]
-}
+               	"shirts": [{
+               			"name": "Crewneck Sweatshirt",
+               			"price": "30.00",
+               			"image": "CrewSweatshirt_740x.jpeg"
+               		},
+               		{
+               			"name": "Tailgate Fest Baseball Tee",
+               			"price": "25.00",
+               			"image": "TailgateFestTee-front_740x.jpeg"
+               		},
+               		{
+               			"name": "2020 Event (Cancelled) Tee",
+               			"price": "15.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "2019 Event Tee",
+               			"price": "10.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "2019 Youth Event Tee",
+               			"price": "10.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Womens Tank Top",
+               			"price": "20.00",
+               			"image": ""
+               		}
+               	],
+               	"hats": [{
+               			"name": "B&W Baseball Cap",
+               			"price": "20.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "Black Flatbill Cap",
+               			"price": "",
+               			"image": ""
+               		},
+               		{
+               			"name": "Trucker Visor",
+               			"price": "15.00",
+               			"image": ""
+               		}
+               	],
+               	"misc": [{
+               			"name": "S4S Facemask",
+               			"price": "10.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Sunglasses",
+               			"price": "5.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Fanny Pack",
+               			"price": "10.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Insulated Tumbler",
+               			"price": "20.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Insulated Convertible Cup",
+               			"price": "20.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Decals (3)",
+               			"price": "5.00",
+               			"image": ""
+               		},
+               		{
+               			"name": "S4S Decals (1)",
+               			"price": "2.00",
+               			"image": ""
+               		}
+               	]
+               }
